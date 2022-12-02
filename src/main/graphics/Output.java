67aa2c75b9fe9f4520b72bf6ffc99bf530584b0f
Jakob <105732359+Wolkensteine@users.noreply.github.com>
2022-12-02 14:50:26 +0100

    Add files via upload

---
+++ src/main/graphics/Output.java
@@ -0,0 +1,25 @@
+package main.graphics;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class Output extends JPanel {
+
+	private BufferedImage canvas;
+	
+	private static Output instance = null;
+	
+	public Output(Dimension dimension) {
+		canvas = new BufferedImage((int)dimension.getHeight(), (int)dimension.getHeight(), BufferedImage.TYPE_INT_ARGB);
+	}
+	
+	public static Output getOutput() {
+		if (Output.instance == null) {
+			Output.instance = new Output(Window.getDimension());
+		}
+		return Output.instance;
+	}
+	
+}
