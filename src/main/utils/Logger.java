948b498e670d0cb527b61170ffe28b56529a54d9
Wolkensteine <bokaj.bb22@gmail.com>
2022-12-02 15:09:12 +0100

    Logger

--- src/main/utils/Logger.java
+++ src/main/utils/Logger.java
@@ -10,7 +10,7 @@ public class Logger {
 
 	public static boolean log(String Module, String Message) {
 		
-		// Variables
+		// Declaration of variables
 		FileWriter LogWriter;
 		LocalDate Date = LocalDate.now();
 		LocalDateTime DateTime = LocalDateTime.now();
