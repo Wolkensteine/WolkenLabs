7e818002bad42e1f6d4bbb047805e56d7e6f8eb4
Wolkensteine <bokaj.bb22gmail.com>
2022-12-02 15:03:55 +0100

    Adding code to logger

--- src/main/utils/Logger.java
+++ src/main/utils/Logger.java
@@ -1,11 +1,57 @@
 package main.utils;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Logger {
 
-	public static void log(String message) {
+	public static boolean log(String Module, String Message) {
 		
-		System.out.println("Logging: " + message);
+		FileWriter LogWriter;
+		LocalDate Date = LocalDate.now();
+		LocalDateTime DateTime = LocalDateTime.now();
+		File LoggingFile = new File("Logs/" + Date + ".txt");
 		
+		try {
+			
+			if (LoggingFile.createNewFile()) {		
+				
+				log("Main > Logger >", "Created Log file!");
+						
+				try {
+					LogWriter = new FileWriter("Logs/" + Date + ".txt", true);				// Creating FileWriter
+					LogWriter.write(DateTime + "=> " + Module + " " + Message + "\n");		// Logs the message
+					System.out.println(DateTime + "=> " + Module + " " + Message + "\n");	// Message is printed out for development
+					LogWriter.close();
+				} catch (IOException e) {
+					System.out.println("An error occurred logging to file.");
+					e.printStackTrace();
+				}
+			
+			} else {
+			
+				try {
+					LogWriter = new FileWriter("Logs/" + Date + ".txt", true);				// Creating FileWriter
+					LogWriter.write(DateTime + "=> " + Module + " " + Message + "\n");		// Logs the message
+					System.out.println(DateTime + "=> " + Module + " " + Message + "\n");	// Message is printed out for development
+					LogWriter.close();
+				} catch (IOException e) {
+					System.out.println("An error occurred logging to file. Error message: " + e);
+					e.printStackTrace();
+				}
+		
+			}
+			return true;
+			
+		} catch (IOException e) {
+			System.out.println("An error occurred. Message: " + e);
+			e.printStackTrace();
+			return false;
+		}
+	
 	}
 	
 }
